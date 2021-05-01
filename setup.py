2969a02809b2dee09f3429a467091dff7daf4657
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2021-05-01 17:21:08 +0700

    feat: initialize ros2 package

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+package_name = 'settei'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools'
+    ],
+    zip_safe=True,
+    maintainer='Segara Bhagas Dagsapurwa',
+    maintainer_email='segara2410@gmail.com',
+    description='Configuration server project',
+    license='MIT License',
+    tests_require=['pytest'],
+)
